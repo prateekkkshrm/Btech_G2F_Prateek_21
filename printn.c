9feec0f941c67f81b2f637c9a43def5d57ff6b32
prateekkkshrm <prateekshrm03@gmail.com>
2026-03-17 12:23:46 +0530

    Create printn.c

---
+++ printn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n=0,i;
+    printf("enter the no.");
+    scanf("%d", &n);
+    printf("n natural no.s are");
+    for(i=1; i<=n; i++)
+    
+    {
+        
+        printf("\n %d \n", i);
+    }
+    return 0;
+}
