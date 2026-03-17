900f500cc1b96efed4f11bc83edc1a922c14dd32
prateekkkshrm <prateekshrm03@gmail.com>
2026-03-17 12:21:55 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  int n, count = 0;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+ 
+
+  do {
+    n /= 10;
+    ++count;
+  } while (n != 0);
+
+  printf("Number of digits: %d", count);
+}
