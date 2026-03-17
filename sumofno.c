b7604089205fb28644256189bb03daac51468a75
prateekkkshrm <prateekshrm03@gmail.com>
2026-03-17 12:24:59 +0530

    Create sumofno.c

---
+++ sumofno.c
@@ -0,0 +1,18 @@
+//WAP to find sum of natural nos. upto N  numbers
+#include <stdio.h>
+
+int main() {
+  int n, i, sum = 0;
+
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; ++i) {
+    sum += i; // equivalent to sum = sum + i;
+  }
+
+  printf("Sum of first %d natural numbers = %d\n", n, sum);
+
+  return 0;
+}
+
